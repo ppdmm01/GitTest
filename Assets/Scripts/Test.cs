6e8eb56c30bcb77aa7de6452ad5f95cc0b9f25e3
ppdmm01 <2164683863@qq.com>
2025-02-07 22:08:40 +0800

    第二次修改

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -8,7 +8,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Second");
     }
 
     // Update is called once per frame
